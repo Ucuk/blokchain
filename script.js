c70dc1e9bc2996e763b9352296917890e1aba148
Ucuk <138536893+Ucuk@users.noreply.github.com>
2024-10-03 20:52:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
 const contractABI = /* masukkan ABI contract Anda */;
-const contractAddress = '0x...';  // Masukkan alamat contract yang telah di-deploy
+const contractAddress = '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4';  // Masukkan alamat contract yang telah di-deploy
 
 const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID'));
 const contract = new web3.eth.Contract(contractABI, contractAddress);
