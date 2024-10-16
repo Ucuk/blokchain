5e711bb75fe2bc1acbd6bcd9ec65f53a043898b3
Ucuk <138536893+Ucuk@users.noreply.github.com>
2024-10-16 09:23:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const Web3 = require('web3');
-const contractABI = /* masukkan ABI contract Anda */;
-const contractAddress = '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4';  // Masukkan alamat contract yang telah di-deploy
+document.getElementById('uploadForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Mencegah reload halaman
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID'));
-const contract = new web3.eth.Contract(contractABI, contractAddress);
-
-async function storeCertificateHash(fileHash, account) {
-    try {
-        const receipt = await contract.methods.storeCertificateHash(fileHash).send({ from: account });
-        console.log('Transaction receipt: ', receipt);
-    } catch (err) {
-        console.error('Error storing certificate hash: ', err);
+    var inputFile = document.getElementById('sertifikatInput').files[0]; // Mendapatkan file
+    if (!inputFile) {
+        alert("Tidak ada file yang dipilih!");
+        return;
     }
-}
 
-module.exports = { storeCertificateHash };
+    var fileReader = new FileReader(); 
+
+    // Membaca file sebagai array buffer untuk hashing
+    fileReader.onload = function(e) {
+        var arrayBuffer = e.target.result; // Buffer untuk hash
+        var spark = new SparkMD5.ArrayBuffer(); // Inisialisasi SparkMD5
+        
+        spark.append(arrayBuffer); // Tambahkan buffer untuk di-hash
+        var hashHex = spark.end(); // Dapatkan hasil hash MD5
+
+        document.getElementById('hashResult').textContent = hashHex; // Tampilkan hasil hash
+    };
+
+    // Jika file adalah gambar, tampilkan di halaman
+    var imgElement = document.getElementById('sertifikatImg');
+    if (inputFile.type.startsWith("image/")) {
+        var imgReader = new FileReader();
+        imgReader.onload = function(e) {
+            imgElement.src = e.target.result;
+        };
+        imgReader.readAsDataURL(inputFile); // Baca gambar sebagai DataURL
+    } else {
+        imgElement.src = "sert2.jpg"; // Jika bukan gambar, tampilkan gambar default
+    }
+
+    fileReader.readAsArrayBuffer(inputFile); // Memulai pembacaan file untuk hashing
+});
